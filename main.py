a7ca3ae28e901695eed7202460425a504229a286
devbenpiacenti <devbenpiacenti@gmail.com>
2025-03-21 11:11:58 -0400

    Fix bug with for loop in incorrect spot

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import turtle
 
 is_race_on = False
 screen = Screen()
-screen.setup(500, 400)
+screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle do you think will win? Enter a color")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
 
-for turtle_index in range(0,6):
+for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.pu()
     new_turtle.color(colors[turtle_index])
@@ -21,17 +21,18 @@ if user_bet:
     is_race_on = True
 
 while is_race_on:
-    if turtle.xcor() > 230:
-        is_race_on = False
-        # print(turtle.color)
-        winning_color = turtle.pencolor()
-        if winning_color == user_bet:
-            print("Your turtle won!")
-        else:
-            print("Your turtle lost.")
-
     for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            print(turtle.pencolor())
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print("Your turtle won!")
+            else:
+                print("Your turtle lost.")
+
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
+        print(turtle.xcor())
     
 screen.exitonclick()
